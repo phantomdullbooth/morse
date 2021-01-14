0c60d906ae2789528ee68901116c7fa6cb038c2c
Addison <49768250+winslowdev@users.noreply.github.com>
2021-01-14 10:19:46 -0500

    set up WatermelonDB, hopefully correctly

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import { Database } from '@nozbe/watermelondb'
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite'
+
+import locations from './models/locations'
+
+const adapter = new SQLiteAdapter({
+    locations,
+    dbName: 'locations',
+    synchronous: true
+})
+
+const database = new Database({
+    adapter,
+    modelClasses: [
+
+    ],
+    actionsEnabled: true,
+})
+
 AppRegistry.registerComponent(appName, () => App);
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  morse
+//
+//  Created by Addison on 1/14/21.
+//
+
+import Foundation
